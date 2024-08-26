62a780fa8eda8ee1a1fcc085254cb2ba92209dfc
seunayolu <seunayolu@gmail.com>
2024-08-27 00:07:41 +0100

    file structure updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-const { upload, processForm } = require('./process'); // Import the process functions
+const { upload, processForm } = require('./process');
 
 // Load environment variables
 dotenv.config();
@@ -17,8 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 // Handle form submissions
 app.post('/submit-form', upload.single('attachment'), processForm);
 
+// Serve the index.html file at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
